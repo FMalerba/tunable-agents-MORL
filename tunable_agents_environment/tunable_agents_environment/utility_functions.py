0970ef23e421b982478dd18195bc6bf85cdb6189
FMalerba <malerbafede@gmail.com>
2020-12-12 14:27:43 +0100

    Initial utility_functions

---
+++ tunable_agents_environment/tunable_agents_environment/utility_functions.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def linear_utility(reward: np.ndarray, weights: np.ndarray):
+    return np.dot(reward, weights)
+
+
+def polinomial_utility(reward: np.ndarray, coefficients: np.ndarray):
+    poly = np.expand_dims(reward, axis=1).repeat(coefficients.shape[1], axis=1) ** np.arange(coefficients.shape[1])
+    return np.sum(poly * coefficients)
+
+
+def threshold_utility(reward: np.ndarray, thresholds: np.ndarray):
+    return np.where(reward >= thresholds, reward, 0)
+
