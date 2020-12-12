e3e46a914f95b66847591e108bcbeee59bc7e58e
FMalerba <malerbafede@gmail.com>
2020-12-12 14:26:58 +0100

    Initialized setup

---
+++ tunable_agents_environment/setup.py
@@ -0,0 +1,27 @@
+from skbuild import setup
+from pkg_resources import parse_requirements
+import pathlib
+
+# Reading README.md to be passed to setup()
+with open("README.md", 'r') as file:
+    long_description = file.read()
+
+# Parsing requirements from requirements.txt;  these will then be passed to setup()
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_reqs = [str(req) for req in parse_requirements(requirements_txt)]
+
+
+
+setup(
+    name='tunable_agents_environment',
+    version='0.0.1',
+    description="Some description",
+    author='Federico Malerba',
+    author_email='malerbafede@gmail.com',
+    url='https://github.com/FMalerba/tunable-agents-MORL',
+    long_description=long_description, 
+    long_description_content_type="text/markdown",
+    install_requires=install_reqs,
+    classifiers=[" Programming Language :: Python :: 3.7", 
+                 "Licence :: OSI Approved :: some_license"]
+)
