46f923a6273384569018babfda70cfd208d280d7
FMalerba <malerbafede@gmail.com>
2020-12-19 19:02:58 +0100

    Fixes and introduction of a functioning observation stacker

--- tunable_agents_environment/tunable_agents_environment/gathering_env/gathering_env.py
+++ tunable_agents_environment/tunable_agents_environment/gathering_env/gathering_env.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from gym_mo.envs.gridworlds import mo_gathering_env
 from tunable_agents_environment import utility_functions
 from tf_agents.environments import py_environment
@@ -6,69 +7,54 @@ from tf_agents.trajectories import time_step as ts
 import gin.tf
 
 from tf_agents.typing import types
-from collections.abc import Callable
-from typing import Tuple, List, Union
 
 import numpy as np
 
 
 
 class ObservationStacker(object):
-    """Class for stacking agent observations."""
+    """
+    Class for stacking agent observations.
+    Observations are stacked along their last dimension; i.e. three 8x8 rgb images (8x8x3)
+    would result in a 8x8x9 observation stack.
+    """
 
-    def __init__(self, history_size: int, observation_size: int, num_players):
+    def __init__(self, history_size: int, observation_shape: Tuple[int]):
         """Initializer for observation stacker.
 
         Args:
           history_size: int, number of time steps to stack.
-          observation_size: int, size of observation vector on one time step.
-          num_players: int, number of players.
+          observation_size: tuple of integers, shape of observation vector on one time step.
         """
         self._history_size = history_size
-        self._observation_size = observation_size
-        self._num_players = num_players
-        self._obs_stacks = list()
-        for _ in range(0, self._num_players):
-            self._obs_stacks.append(np.zeros(self._observation_size *
-                                             self._history_size))
+        self._observation_shape = observation_shape
+        self._obs_stack : np.ndarray = np.zeros(shape=(*observation_shape[:-1], observation_shape[-1]*history_size))
 
-    def add_observation(self, observation, current_player):
+    def add_observation(self, observation: np.ndarray):
         """Adds observation for the current player.
 
         Args:
-          observation: observation vector for current player.
-          current_player: int, current player id.
+          observation: observations to be added to the stack.
         """
-        self._obs_stacks[current_player] = np.roll(self._obs_stacks[current_player],
-                                                   -self._observation_size)
-        self._obs_stacks[current_player][(self._history_size - 1) *
-                                         self._observation_size:] = observation
+        self._obs_stack = np.roll(self._obs_stack, -self._observation_shape[-1], axis=-1)
+        self._obs_stack[..., -self._observation_shape[-1]:] = observation
 
-    def get_observation_stack(self, current_player):
-        """Returns the stacked observation for current player.
-
-        Args:
-          current_player: int, current player id.
-        """
-
-        return self._obs_stacks[current_player]
+    def get_observation_stack(self):
+        """Returns the stacked observations."""
+        return self._obs_stack
 
     def reset_stack(self):
         """Resets the observation stacks to all zero."""
-
-        for i in range(0, self._num_players):
-            self._obs_stacks[i].fill(0.0)
+        self._obs_stack.fill(0.0)
 
     @property
     def history_size(self):
         """Returns number of steps to stack."""
         return self._history_size
 
-    def observation_size(self):
-        """Returns the size of the observation vector after history stacking."""
-        return self._observation_size * self._history_size
-
-
+    def observation_shape(self):
+        """Returns the shape of the observations after history stacking."""
+        return self._obs_stack.shape
 
 
 @gin.configurable(blacklist=['environment'])
@@ -84,9 +70,7 @@ def create_obs_stacker(environment: py_environment.PyEnvironment, history_size: 
     """
 
     return ObservationStacker(history_size,
-                              environment.observation_spec().shape()[0],
-                              num_players=1)
-
+                              environment.observation_spec()['observations'].shape())
 
 
 
@@ -95,11 +79,13 @@ class Gathering_wrapper(py_environment.PyEnvironment):
     def __init__(self, preference: np.ndarray = None,
                  gamma: float = 0.99, history_size: int = 3) -> None:
         super().__init__()
+        # If a preference is passed to the environment, then such preference is fixed and won't be resampled
+        self._fixed_preference = bool(preference)
         self._preference = preference
         self._env = mo_gathering_env.MOGatheringEnv()
         self.gamma = gamma
         self._obs_stacker = create_obs_stacker(self, history_size=history_size)
-        self._observation_spec = {'observations': array_spec.ArraySpec(shape=(2,), dtype=np.int64),
+        self._observation_spec = {'observations': array_spec.ArraySpec(shape=self._obs_stacker.observation_shape(), dtype=np.float32),
                                   'preference_weights': array_spec.ArraySpec(shape=(6,), dtype=np.float32)}
         self._action_spec = array_spec.BoundedArraySpec(shape=(), dtype=np.int_, minimum=0, maximum=4)
     
@@ -113,11 +99,17 @@ class Gathering_wrapper(py_environment.PyEnvironment):
     
     
     def _reset(self) -> ts.TimeStep:
-        if self._preference:
+        if self._fixed_preference:
             obs = self._env.reset(self._preference)
         else:
-            obs = self._env.reset(utility_functions.sample_preference())
-        observations_and_preferences = {'observations': obs,
+            self._preference = utility_functions.sample_preference()
+            obs = self._env.reset()
+        
+        self._obs_stacker.reset_stack()
+        self._obs_stacker.add_observation(obs)
+        stacked_obs = self._obs_stacker.get_observation_stack()
+        
+        observations_and_preferences = {'observations': stacked_obs,
                                         'preference_weights': self._preference/40}
         return ts.restart(observations_and_preferences)
     
@@ -128,8 +120,11 @@ class Gathering_wrapper(py_environment.PyEnvironment):
         
         obs, rewards, done = self._env.step(action)
         
+        self._obs_stacker.add_observation(obs)
+        stacked_obs = self._obs_stacker.get_observation_stack()
+        
         # Preferences are in range [-20, 20] we normalize them to the range [-0.5, 0.5]
-        observations_and_preferences = {'observations': obs,
+        observations_and_preferences = {'observations': stacked_obs,
                                         'preference_weights': self._preference/40}
         
         reward = np.dot(rewards, self._preference)
