72bd2519c6ffdb86f9540aa17dbf684b1c6b53db
FMalerba <malerbafede@gmail.com>
2020-12-23 14:05:36 +0100

    Fix

--- tunable_agents/external_configurables.py
+++ tunable_agents/external_configurables.py
@@ -1,8 +1,7 @@
 from gin import config
 import tensorflow as tf
 
-# config.external_configurable(tf.keras.layers.Dense, name='Dense', module='tf.keras.layers')
-config.external_configurable(tf.keras.layers.Conv2D, name='Dense')
+config.external_configurable(tf.keras.layers.Conv2D, name='Conv2D')
 config.external_configurable(tf.keras.layers.Flatten, name='Flatten')
-config.external_configurable(tf.keras.Sequential, name='Sequential')
+#config.external_configurable(tf.keras.Sequential, name='Sequential')
 config.external_configurable(tf.keras.layers.Dropout, name='Dropout')
