e31f1d1cd6e6831521024d73c52cc5daa223715b
FMalerba <malerbafede@gmail.com>
2020-12-26 19:17:32 +0100

    Fixes and reorganization of the DST environment

--- tunable_agents/environments/DST_env/DST_env.py
+++ tunable_agents/environments/DST_env/DST_env.py
@@ -27,12 +27,10 @@ class DeepSeaTreasureEnvironment:
             (i, j) for j in range(GRID_COLS) for i in range(TREASURE_DEPTHS[j] + 1, GRID_ROWS)
         ]
         self._treasure_locations = set([(i, j) for j, i in enumerate(TREASURE_DEPTHS)])
-        self._utility_function: Callable[[np.ndarray, np.ndarray], np.ndarray] = None
 
     def reset(self) -> Tuple[List[int], Tuple[bool]]:
         self._n_steps = 0
         self._state = [0, 0]
-        self._utility_function = utility_functions.generate_utility()
         legal_moves = self._legal_moves()
         return self._state, legal_moves
 
@@ -97,8 +95,11 @@ class DSTWrapper(py_environment.PyEnvironment):
         super().__init__()
         self._env = DeepSeaTreasureEnvironment()
         self.gamma = gamma
+        self._utility_function: Callable[[np.ndarray, np.ndarray], np.ndarray] = None
+        self._cumulative_rewards: np.ndarray = np.zeros(shape=(2,) dtype=np.float32)
         self._observation_spec = {
             'observations': array_spec.ArraySpec(shape=(2,), dtype=np.int64),
+            'cumulative_rewards': array_spec.ArraySpec(shape=(2,), dtype=np.float32,
             'legal_moves': array_spec.ArraySpec(shape=(4,), dtype=np.bool_)
         }
         self._action_spec = array_spec.BoundedArraySpec(shape=(),
@@ -114,7 +115,11 @@ class DSTWrapper(py_environment.PyEnvironment):
 
     def _reset(self) -> ts.TimeStep:
         obs, legal_moves = self._env.reset()
-        observations_and_legal_moves = {'observations': obs, 'legal_moves': legal_moves}
+        self._cumulative_rewards.fill(0.0)
+        self._utility_function = utility_functions.generate_utility()
+        observations_and_legal_moves = {'observations': obs,
+                                        'cumulative_rewards': self._cumulative_rewards,
+                                        'legal_moves': legal_moves}
         return ts.restart(observations_and_legal_moves)
 
     def _step(self, action: types.NestedArray) -> ts.TimeStep:
@@ -122,10 +127,13 @@ class DSTWrapper(py_environment.PyEnvironment):
             return self.reset()
 
         obs, legal_moves, rewards, done = self._env.step(action)
+        self._cumulative_rewards += rewards
 
-        observations_and_legal_moves = {'observations': obs, 'legal_moves': legal_moves}
+        observations_and_legal_moves = {'observations': obs,
+                                        'cumulative_rewards': self._cumulative_rewards,
+                                        'legal_moves': legal_moves}
 
-        # TODO Manage the reward-utility
+        reward = self._utiility_function(self._cumuluative_rewards)
         if done:
             return ts.termination(observations_and_legal_moves, reward)
         else:
