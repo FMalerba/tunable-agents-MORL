9206c6b7bacd2c3ff159b73ace099e32b2f7be0d
FMalerba <malerbafede@gmail.com>
2021-01-01 14:41:46 +0100

    Restructuring of the code

--- tunable_agents/main.py
+++ tunable_agents/main.py
@@ -33,35 +33,36 @@ FLAGS = flags.FLAGS
 
 
 @gin.configurable(allowlist=['collect_episodes'])
-def initial_collection(agent: tf_agent.TFAgent, env: tf_py_environment.TFPyEnvironment, rb_observer: list,
-                       collect_episodes: int) -> None:
-    """
-    Samples transitions with the given agent and environment without training
-    on the experience collected. This is done to partially fill-up the Replay Buffer.
-    """
+def collection_step(driver: dynamic_episode_driver.DynamicEpisodeDriver, collect_episodes: int) -> None:
+    """Samples transitions with the given Driver."""
     if not collect_episodes:
         return
-    collect_policy = agent.collect_policy
-    print('Sampling {} episodes with no training'.format(collect_episodes))
+
+    print('Sampling {} episodes with the Driver'.format(collect_episodes))
     start_time = time.time()
-    dynamic_episode_driver.DynamicEpisodeDriver(env,
-                                                collect_policy,
-                                                observers=rb_observer,
-                                                num_episodes=collect_episodes).run()
+    driver.run(num_episodes=collect_episodes)
     print('Finished sampling with the Driver, it took {} seconds for {} episodes\n'.format(
         time.time() - start_time, collect_episodes))
 
 
 @gin.configurable(allowlist=[])
-def initial_training(agent_train_function: Callable,
-                     replay_buffer: tf_uniform_replay_buffer.TFUniformReplayBuffer, num_steps: int,
-                     train_steps: int, batch_size: int) -> None:
+def training_step(agent_train_function: Callable,
+                  replay_buffer: tf_uniform_replay_buffer.TFUniformReplayBuffer, train_steps: int) -> None:
     if not train_steps:
         return
+    """
+    TODO Performance Optimization: Try out different batch sizes (TF usually recommends higher batch size) 
+        and see how this influences performance, keeping track of possible differences in RAM/VRAM requirements.
+        To do this properly the variable train_steps_per_epoch should be changed appropriately (e.g. double
+        the batch size --> half the train_steps), but it would be nice to also check that this behaves
+        as expected and doesn't impact per-epoch-learning. Per-epoch-learning is an abstract metric I just
+        invented that would tell you how much better a model got after an epoch... Essentially one should
+        check that the agent manages to reach the same level of performance (measured perhaps in 
+        average_return_per_episode == number of fireworks placed) at the same epoch (more or less) even if
+        you do this thing of doubling batch_size and halving train_steps_per_epoch.
+    """
     # Dataset generates trajectories with shape [Bx2x...]
-    dataset = replay_buffer.as_dataset(
-        num_parallel_calls=3, sample_batch_size=batch_size,
-        num_steps=num_steps).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    dataset = replay_buffer.as_dataset().prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
     print('Starting partial training of Agent from Replay Buffer' '\nCounting Steps:')
     # Commenting out losses (and all their relevant code) to try and see if they are responsible for an observed memory leak.
     # No feedback available yet on whether this is the case or not
@@ -83,7 +84,7 @@ def initial_training(agent_train_function: Callable,
             graph-mode executions). This will stop working either if we start to export the compiled functions
             so that we don't have the first "pythonic" run of them or if for some reason we change the file_writer
             during execution. Should the summary writer be passed to the agent training function so that it can be set 
-            as default from inside the boundary of tf.function? How does tf-agent solve this issue?
+            as default from inside the boundary of tf.function? How does TF-Agent solve this issue?
         """
 
         # losses = losses.write(c, agent_train_function(experience=experience).loss)
@@ -100,13 +101,6 @@ def train_eval(
         experiment_name: str,
         env_id: str,  # Used to identify a unique environment for RB reuse
         num_iterations: int,
-        # Param for collect
-        collect_episodes_per_epoch: int,
-        # Number of steps for training update
-        num_steps: int,
-        # Params for train
-        train_steps_per_epoch: int,
-        batch_size: int,
         # Params for eval
         eval_interval: int,
         num_eval_episodes: int,
@@ -145,10 +139,7 @@ def train_eval(
         if num_steps=2 (as right now) then a frame is  [time_step, action, next_time_step] 
         (where time_step has all info including last reward). If you increase num_steps then 
         it's obvious how a frame would change, and also how this affects the *actual* number of transitions
-        that the RB is storing. Also note that if I ever actually manage to do the Prioritized RB,
-        it won't support this batch parallelization. The issue lies with the SumTree object 
-        (which I imported from the DeepMind framework) and the fact that it doesn't seem to me
-        like this object could be parallelized (meaning that all memory access issues are solved) in any way...
+        that the RB is storing.
     """
     # create the enviroment
     env = utility.create_environment()
@@ -171,12 +162,9 @@ def train_eval(
         See https://www.tensorflow.org/guide/keras/mixed_precision for more info
     """
     # create an agent and a network
-    tf_agent = utility.create_agent(
-        environment=tf_env,
-        # num_steps parameter must differ by 1 between agent and replay_buffer.as_dataset() call
-        n_step_update=num_steps - 1,
-        decaying_epsilon=decaying_epsilon,
-        train_step_counter=train_step)
+    tf_agent = utility.create_agent(environment=tf_env,
+                                    decaying_epsilon=decaying_epsilon,
+                                    train_step_counter=train_step)
 
     # replay buffer
     replay_buffer = utility.create_replay_buffer(data_spec=tf_agent.collect_data_spec,
@@ -186,27 +174,27 @@ def train_eval(
     train_metrics = [
         tf_metrics.NumberOfEpisodes(),
         tf_metrics.EnvironmentSteps(),
-        tf_metrics.AverageReturnMetric(buffer_size=collect_episodes_per_epoch),
-        tf_metrics.AverageEpisodeLengthMetric(buffer_size=collect_episodes_per_epoch),
+        tf_metrics.AverageReturnMetric(),
+        tf_metrics.AverageEpisodeLengthMetric(),
     ]
 
     # replay buffer update for the driver
     replay_observer = [replay_buffer.add_batch]
 
-    eval_metrics = [
-        tf_metrics.AverageReturnMetric(buffer_size=num_eval_episodes),
-        tf_metrics.AverageEpisodeLengthMetric(buffer_size=num_eval_episodes),
-    ]
+    with gin.config_scope('eval_metrics'):
+        eval_metrics = [
+            tf_metrics.AverageReturnMetric(),
+            tf_metrics.AverageEpisodeLengthMetric(),
+        ]
 
     # I create the Driver only once instead of recreating it at the start of every epoch
     # This is not a problem only because the agent's collect_policy has the agent's network
     # and its tensors will update together with the agent's on agent.train() calls.
-    # If the agent is has a policy that doesn't depend only on Tensors, one must recreate a new
+    # If the agent has a policy that doesn't depend only on Tensors, one must recreate a new
     # policy after every epoch and create a new Driver to feed this to.
     collect_driver = dynamic_episode_driver.DynamicEpisodeDriver(tf_env,
                                                                  tf_agent.collect_policy,
-                                                                 observers=replay_observer + train_metrics,
-                                                                 num_episodes=collect_episodes_per_epoch)
+                                                                 observers=replay_observer + train_metrics)
 
     # checkpointer:
     train_checkpointer = common.Checkpointer(ckpt_dir=os.path.join(experiment_dir, 'checkpoints', 'train'),
@@ -250,66 +238,21 @@ def train_eval(
     # Care that JIT only improves performance if stuff doesn't change shapes often
     tf.config.optimizer.set_jit(XLA_flag)
 
-    initial_collection(tf_agent, tf_env, replay_observer)
-    
-    initial_training(agent_train_function=agent_train_function, replay_buffer=replay_buffer)
+    with gin.config_scope('initial_step'):
+        # Initial collection
+        collection_step(driver=collect_driver)
+
+        # Initial training in case the RB was pre-loaded with a checkpoint
+        training_step(agent_train_function=agent_train_function,
+                    replay_buffer=replay_buffer)
 
     for _ in range(num_iterations):
         print('EPOCH {}'.format(epoch_counter.numpy()))
         tf.summary.scalar(name='Epsilon', data=decaying_epsilon(), step=epoch_counter)
-        # episode driver
-        print('\nStarting to run the Driver')
-        start_time = time.time()
-        collect_driver.run()
-        print('Finished running the Driver, it took {} seconds for {} episodes\n'.format(
-            time.time() - start_time, collect_episodes_per_epoch))
-        """
-        TODO Performance Optimization: Try out different batch sizes (TF usually recommends higher batch size) 
-            and see how this influences performance, keeping track of possible differences in RAM/VRAM requirements.
-            To do this properly the variable train_steps_per_epoch should be changed appropriately (e.g. double
-            the batch size --> half the train_steps), but it would be nice to also check that this behaves
-            as expected and doesn't impact per-epoch-learning. Per-epoch-learning is an abstract metric I just
-            invented that would tell you how much better a model got after an epoch... Essentially one should
-            check that the agent manages to reach the same level of performance (measured perhaps in 
-            average_return_per_episode == number of fireworks placed) at the same epoch (more or less) even if
-            you do this thing of doubling batch_size and halving train_steps_per_epoch.
-        """
-        # Dataset generates trajectories with shape [Bx2x...]
-        dataset = replay_buffer.as_dataset(
-            num_parallel_calls=3, sample_batch_size=batch_size,
-            num_steps=num_steps).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
-        print('Starting partial training of Agent from Replay Buffer' '\nCounting Steps:')
-        # Commenting out losses_1/_2 (and all their relevant code) to try and see if they are responsible for an observed memory leak.
-        # No feedback available yet on whether this is the case or not
-        # losses = tf.TensorArray(tf.float32, size=train_steps_per_epoch)
-        c = 0
-        start_time = time.time()
-        for data in dataset:
-            if c % (train_steps_per_epoch / 10) == 0 and c != 0:
-                #tf.summary.scalar("loss_agent", tf.math.reduce_mean(losses.stack()), step=train_step)
-                print("{}% completed with {} steps done".format(int(c / train_steps_per_epoch * 100), c))
-            if c == train_steps_per_epoch:
-                break
-            experience, data_info = data
-            """
-            FIXME tensorflow documentation at https://www.tensorflow.org/tensorboard/migrate states that
-                default_writers do not cross the tf.function boundary and should instead be called as
-                default inside the tf.function. For now our code works because on the first run of the
-                training function the code is run in non-graph mode and thus "sees" the writer (and can
-                then use it even in subsequent graph-mode executions). This will stop working either if
-                we start to export the compiled functions so that we don't have the first "pythonic" run
-                of them or if for some reason we change the file_writer during execution. Should the 
-                summary writer be passed to the agent training function so that it can be set as default
-                from inside the boundary of tf.function? How does tf-agent solve this issue?
-            """
+        collection_step(driver=collect_driver)
 
-            # losses = losses.write(c, agent_train_function(experience=experience).loss)
-            loss_agent_info = agent_train_function(experience=experience)
-            c += 1
-
-        # losses = losses.stack()
-        print("Ended epoch training for agent, it took {}".format(time.time() - start_time))
+        training_step(agent_train_function=agent_train_function, replay_buffer=replay_buffer)
 
         epoch_counter.assign_add(1)
 
