f2071b47bea2508d1501cd8b4b38cf9f52018352
FMalerba <malerbafede@gmail.com>
2021-01-01 16:08:23 +0100

    Added support for conig files include statements that are relative to the 'configs' folder

--- tunable_agents/utility.py
+++ tunable_agents/utility.py
@@ -29,6 +29,13 @@ def load_gin_configs(gin_files, gin_bindings):
       gin_bindings: List of gin parameter bindings to override the values in the
         config files.
     """
+    try:
+        path_folders = gin_files[0].split('/')
+        configs_folder_index = path_folders.index('configs')
+    except:
+        raise ValueError("Expected gin_files paths to be like {}, instead got {}".format('.../configs/...', gin_files[0]))
+    configs_folder_path = '/'.join(path_folders[:configs_folder_index])
+    gin.add_config_file_search_path(configs_folder_path)
     gin.parse_config_files_and_bindings(gin_files, bindings=gin_bindings, skip_unknown=False)
 
 
