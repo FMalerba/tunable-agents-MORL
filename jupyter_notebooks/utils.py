ac90ee4c2b857a46db8b6191924dc9f59fc8e22a
FMalerba <malerbafede@gmail.com>
2021-02-18 09:35:08 +0100

    Notebook updated and added a new notebook to better debug the environment

--- jupyter_notebooks/utils.py
+++ jupyter_notebooks/utils.py
@@ -1,42 +1,58 @@
 import numpy as np
-from tf_agents.utils import common
 from tf_agents.trajectories import time_step as ts
-from tf_agents.typing import types
-from tf_agents.environments import tf_py_environment
+from tf_agents.environments import py_environment
 from tf_agents.policies import tf_policy
 import matplotlib.pyplot as plt
-import time
 
 
-def run_and_render_episode(env: tf_py_environment.TFPyEnvironment, policy: tf_policy.TFPolicy) -> None:
+def render_time_step(time_step: ts.TimeStep, ax, action: int = None) -> None:
+    image = time_step.observation['state_obs'][:, :, -3:]
+    ax.imshow(image)
+    ax.set_title('Utility: {}    Action: {}'.format(time_step.reward, action), fontsize=5)
+    ax.axis('off')
+
+
+def policy_play_episode(env: py_environment.PyEnvironment, policy: tf_policy.TFPolicy) -> None:
     time_step = env.reset()
     policy_state = policy.get_initial_state(1)
     
     plt.figure(figsize=(11, 6), dpi=200)
-    i = 0
-    render_time_step(time_step, i=i)
+    i = 1
+    ax = plt.subplot(4, 8, i)
+    render_time_step(time_step, ax)
     while not time_step.is_last():
         action_step = policy.action(time_step, policy_state)
         time_step = env.step(action_step.action)
         policy_state = action_step.state
         i += 1
-        render_time_step(time_step, action_step.action, i)
+        ax = plt.subplot(4, 8, i)
+        render_time_step(time_step, ax, action_step.action)
     
     plt.tight_layout()
     plt.show()
 
 
-def render_time_step(time_step: ts.TimeStep, action: int = None, i: int = 0) -> None:
-    plt.subplot(4, 8, i+1)
-    image = time_step.observation['state_obs'][:, :, -3:]
-    plt.imshow(image)
-    plt.title('Utility: {}    Action: {}'.format(time_step.reward, action), fontsize=5)
-    plt.axis('off')
-    #plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
-    #plt.tick_params(axis='y', which='both', left=False, right=False, labelleft=False)
+def model_play_episode(env: py_environment.PyEnvironment, model) -> None:
+    time_step = env.reset()
+        
+    plt.figure(figsize=(11, 6), dpi=200)
+    i = 1
+    ax = plt.subplot(4, 8, i)
+    render_time_step(time_step, ax)
+    while not time_step.is_last():
+        state = time_step.observation['state_obs']
+        weights = time_step.observation['utility_representation'][2:]
+        action = np.argmax(model.predict([state[np.newaxis], weights[np.newaxis]]))
+        time_step = env.step(action)
+        i += 1
+        ax = plt.subplot(4, 8, i)
+        render_time_step(time_step, ax, action)
+    
+    plt.tight_layout()
+    plt.show()
 
 
-def evaluate_average_utility(env: tf_py_environment.TFPyEnvironment, policy: tf_policy.TFPolicy, n_episodes: int) -> float:
+def evaluate_average_utility(env: py_environment.PyEnvironment, policy: tf_policy.TFPolicy, n_episodes: int) -> float:
     utilities = np.empty(shape=(n_episodes), dtype=np.float32)
     
     for i in range(n_episodes):
