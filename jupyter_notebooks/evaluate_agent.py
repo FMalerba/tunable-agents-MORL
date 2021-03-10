c6522c7b33b9398fce69b548e9edc0c2b79d2495
FMalerba <malerbafede@gmail.com>
2021-03-10 20:23:45 +0100

    Added support for reward_vector results instead of only utility scalarized ones. Also general cleanup following main.py on previous commit.

--- jupyter_notebooks/evaluate_agent.py
+++ jupyter_notebooks/evaluate_agent.py
@@ -9,24 +9,7 @@ from tqdm import tqdm
 import gin
 
 from tunable_agents import utility, agent
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 from tf_agents.environments import py_environment
-
-#gpus = tf.config.experimental.list_physical_devices('GPU')
-#tf.config.experimental.set_memory_growth(gpus[0], True)
-
-flags.DEFINE_string('experiment_dir', os.getenv('TEST_UNDECLARED_OUTPUTS_DIR'),
-                    'Root directory for writing logs/summaries/checkpoints.')
-flags.DEFINE_string('results_dir', os.getenv('TEST_UNDECLARED_OUTPUTS_DIR'),
-                    'Root directory for writing logs/summaries/checkpoints.')
-flags.DEFINE_multi_string('gin_files', [], 'List of paths to gin configuration files (e.g.'
-                          '"configs/hanabi_rainbow.gin").')
-flags.DEFINE_multi_string(
-    'gin_bindings', [], 'Gin bindings to override the values set in the config files '
-    '(e.g. "train_eval.num_iterations=100").')
-flags.DEFINE_integer('n_episodes', 10_000, "Number of episodes to evaluate the agent on")
-
-FLAGS = flags.FLAGS
 
 
 @gin.configurable
@@ -35,9 +18,9 @@ def train_eval(training_id: str,model_id: str,env_id: str):
 
 
 def eval_agent(env: py_environment.PyEnvironment, tf_agent: agent.DQNAgent,
-               n_episodes: int) -> np.ndarray:
-    utilities = []
-
+               n_episodes: int, reward_vector: bool=False) -> np.ndarray:
+    
+    results = []
     for _ in tqdm(range(n_episodes)):
         ts = env.reset()
         observations = ts.observation
@@ -52,9 +35,12 @@ def eval_agent(env: py_environment.PyEnvironment, tf_agent: agent.DQNAgent,
             episode_reward += reward
         
         assert episode_reward == env._prev_step_utility
-        utilities.append(episode_reward)
+        if reward_vector:
+            results.append(env._cumulative_rewards)
+        else:
+            results.append(episode_reward)
     
-    return utilities
+    return results
 
 
 def main(_):
@@ -73,18 +59,36 @@ def main(_):
     
     tf_agent.load_model(model_path)
     
-    results = eval_agent(env, tf_agent, FLAGS.n_episodes)
+    results = eval_agent(env, tf_agent, FLAGS.n_episodes, FLAGS.reward_vector)
     
+    results_dir = os.path.join(FLAGS.results_dir, "reward_vector") if FLAGS.reward_vector else FLAGS.results_dir
+    if not os.path.exists():
+        os.makedirs(results_dir)
     
-    if not os.path.exists(FLAGS.results_dir):
-        os.makedirs(FLAGS.results_dir)
-    
-    results_path = FLAGS.results_dir + experiment_dir.split("/")[-1] + ".npy"
+    results_path = results_dir + experiment_dir.split("/")[-1] + ".npy"
     if os.path.exists(results_path):
         results = np.append(np.load(results_path), results, axis=0)
     np.save(results_path, results)
 
 
 if __name__ == '__main__':
+    flags.DEFINE_string('experiment_dir', os.getenv('TEST_UNDECLARED_OUTPUTS_DIR'),
+                        'Directory where the agent is saved.')
+    flags.DEFINE_string('results_dir', os.getenv('TEST_UNDECLARED_OUTPUTS_DIR'),
+                        'Root directory for writing the results')
+    flags.DEFINE_multi_string('gin_files', [], 'List of paths to gin configuration files (e.g.'
+                            '"configs/hanabi_rainbow.gin").')
+    flags.DEFINE_multi_string(
+        'gin_bindings', [], 'Gin bindings to override the values set in the config files '
+        '(e.g. "train_eval.num_iterations=100").')
+    flags.DEFINE_integer('n_episodes', 10_000, "Number of episodes to evaluate the agent on")
+    flags.DEFINE_bool('reward_vector', False, "Whether to compute results for the reward vector"
+                    " instead of the utility scalarized result.")
+    FLAGS = flags.FLAGS
     flags.mark_flag_as_required('experiment_dir')
+    
+    #gpus = tf.config.experimental.list_physical_devices('GPU')
+    #tf.config.experimental.set_memory_growth(gpus[0], True)
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+    
     app.run(main)
