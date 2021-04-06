c4b14b23c3a123935ba7f5787058de2a323ebddf
FMalerba <malerbafede@gmail.com>
2021-04-06 17:01:37 +0200

    Restructuring of the utility functions to account for recent changes in the gathering environment classes. Also added new TargetUtility

--- tunable_agents/environments/utility_functions.py
+++ tunable_agents/environments/utility_functions.py
@@ -6,14 +6,20 @@ from typing import Optional
 class UtilityFunction(ABC):
 
     def __init__(self, utility_repr: np.ndarray, agent_utility_repr: np.ndarray,
-                 gridworld_utility_repr: np.ndarray) -> None:
-        """
-        There are two utility representations, one that is fed as input to the agent and the other that is fed as preference
-        representation to the gridworld object underlying the GatheringWrapper environment.
+                 interests: np.ndarray) -> None:
+        """Abstarct class for utility functions to be used in the Gathering environment
+
+        Args:
+            utility_repr (np.ndarray): Complete utility representation.
+            agent_utility_repr (np.ndarray): Utiilty representation to be fed as input to the agent
+                    usually, this will be a flattened version of utility_repr with any unchanging entries
+                    removed (e.g. entries for unchanging weights of time and wall penalties).
+            interests (np.ndarray): Representation of interests of the agent. This will be used by the environment
+                    to determine the end of an episode.
         """
         self._utility_repr = utility_repr
         self._agent_utility_repr = agent_utility_repr
-        self._gridworld_utility_repr = gridworld_utility_repr
+        self._interests = interests
 
     def __call__(self, rewards: np.ndarray) -> np.ndarray:
         return self.call(rewards)
@@ -23,47 +29,40 @@ class UtilityFunction(ABC):
         """Implements the utility function"""
 
     @property
-    def utility_repr(self):
+    def utility_repr(self) -> np.ndarray:
         return self._utility_repr
 
     @property
-    def agent_utility_repr(self):
+    def agent_utility_repr(self) -> np.ndarray:
         return self._agent_utility_repr
 
     @property
-    def gridworld_utility_repr(self):
-        return self._gridworld_utility_repr
+    def interests(self) -> np.ndarray:
+        return self._interests
 
 
 class LinearUtility(UtilityFunction):
 
     def __init__(self,
                  weights: Optional[np.ndarray] = None,
-                 agent_utility_repr: Optional[np.ndarray] = None,
-                 gridworld_utility_repr: Optional[np.ndarray] = None) -> None:
+                 agent_utility_repr: Optional[np.ndarray] = None) -> None:
         """
-        Contains all information and possible different representations of a utility function.
+        Implements and contains all information and possible different representations of a linear utility function.
         
-        At least one of the three optional arguments must be provided in order to reconstruct the others.
+        At least one of the first two optional arguments must be provided in order to reconstruct the others.
         """
         if weights is not None:
             agent_utility_repr = weights[2:] / 40
-            gridworld_utility_repr = weights
+            interests = (weights > 0).astype(int)
         elif agent_utility_repr is not None:
             weights = np.concatenate(([-1, -5], agent_utility_repr * 40)).astype(np.float32)
-            gridworld_utility_repr = weights
-        elif gridworld_utility_repr is not None:
-            weights = gridworld_utility_repr
-            agent_utility_repr = weights[2:] / 40
+            interests = (weights > 0).astype(int)
         else:
-            raise ValueError(
-                "Expected to receive at least one utility representation argument, but received none.")
+            raise ValueError("Expected to receive at least one required argument, but received none.")
 
         self._weights = weights
 
-        super().__init__(utility_repr=weights,
-                         agent_utility_repr=agent_utility_repr,
-                         gridworld_utility_repr=gridworld_utility_repr)
+        super().__init__(utility_repr=weights, agent_utility_repr=agent_utility_repr, interests=interests)
 
     def call(self, rewards: np.ndarray) -> np.ndarray:
         return np.dot(rewards, self._weights)
@@ -73,22 +72,22 @@ class ThresholdUtility(UtilityFunction):
 
     def __init__(self,
                  thresholds_and_ceofficients: Optional[np.ndarray] = None,
-                 agent_utility_repr: Optional[np.ndarray] = None,
-                 gridworld_utility_repr: Optional[np.ndarray] = None) -> None:
+                 agent_utility_repr: Optional[np.ndarray] = None) -> None:
         """
-        Contains all information and possible different representations of a utility function.
+        Implements and contains all information and possible different representations of a threshold utility function.
         
-        It is required that either thresholds_and_ceofficients is provided or the agent_utility_repr in order to reconstruct the others.
+        It is required that either thresholds_and_ceofficients or the agent_utility_repr be provided 
+        in order to reconstruct the others.
         """
         if thresholds_and_ceofficients is not None:
             thresholds = thresholds_and_ceofficients[0]
             coefficients = thresholds_and_ceofficients[1]
             agent_utility_repr = (thresholds_and_ceofficients[:, 2:] / [[1], [40]]).flatten()
-            gridworld_utility_repr = thresholds_and_ceofficients[1]
+            interests = (thresholds_and_ceofficients[1] > 0).astype(int)
         elif agent_utility_repr is not None:
             thresholds = np.concatenate(([0, 0], agent_utility_repr[:4])).astype(np.float32)
             coefficients = np.concatenate(([-1, -5], agent_utility_repr[4:] * 40)).astype(np.float32)
-            gridworld_utility_repr = coefficients
+            interests = (coefficients > 0).astype(int)
         else:
             raise ValueError(
                 "Expected to receive at least one utility representation argument, but received none.")
@@ -97,10 +96,41 @@ class ThresholdUtility(UtilityFunction):
         self._coefficients = coefficients
         super().__init__(utility_repr=np.array([thresholds, coefficients]),
                          agent_utility_repr=agent_utility_repr,
-                         gridworld_utility_repr=gridworld_utility_repr)
+                         interests=interests)
 
     def call(self, rewards: np.ndarray) -> np.ndarray:
         return np.sum(np.where(rewards >= self._thresholds, rewards * self._coefficients, 0))
+
+
+class TargetUtility(UtilityFunction):
+
+    def __init__(self,
+                 target: Optional[np.ndarray] = None,
+                 agent_utility_repr: Optional[np.ndarray] = None) -> None:
+        """
+        Implements and contains all information and possible different representations of a utility function.
+        
+        It is required that either the target is provided or the agent_utility_repr in order to reconstruct the others.
+        """
+        if target is not None:
+            agent_utility_repr = target
+        elif agent_utility_repr is not None:
+            target = agent_utility_repr
+        else:
+            raise ValueError("Expected to receive at least one required argument, but received none.")
+
+        self._target = target
+        interests = target
+        super().__init__(utility_repr=target, agent_utility_repr=agent_utility_repr, interests=interests)
+
+    def call(self, rewards: np.ndarray) -> np.ndarray:
+        # The reward vector is rescaled according to the specs for the Gathering environment.
+        # The first two entries are for time and wall penalties, and as such get multiplied by -1
+        # and subsequently translated so that they are always >= 0.
+        # Episode length is 31 (this is due to unexpected and poorly executed behaviour for the underlying
+        # MOGridworld class) and time and wall penalties are 1 per time step, so the translation ought to be
+        # of 31.
+        return np.min((rewards * [-1, -1, 1, 1, 1, 1] + [31, 31, 0, 0, 0, 0]) / self._target)
 
 
 def polinomial_utility(reward: np.ndarray, coefficients: np.ndarray) -> float:
@@ -169,9 +199,19 @@ def sample_linear_thresholds() -> np.ndarray:
         return sample_thresholds_and_coefficients()
 
     w01 = np.array([-1, -5], dtype=np.float32)
-    return np.array(
-        [[0, 0, 0, 0, 0, 0],
-         np.concatenate((w01, coefficients))], dtype=np.float32)
+    return np.array([[0, 0, 0, 0, 0, 0], np.concatenate((w01, coefficients))], dtype=np.float32)
+
+
+def sample_target() -> np.ndarray:
+    """
+    Samples a 6-long vector of targets for the gathering environment.
+    Each preference weight is randomly sampled between -20 and 20 in steps of 5 with the 
+    exception of the first two entries which are fixed at -1 and -5. They respectively signal
+    the punishment for taking a further time-step and the punishment for hitting a wall
+
+    Returns:
+        target: The target reward vector for the agent to achieve in the gridworld environment
+    """
 
 
 def sample_utility(utility_type: str = 'linear',
@@ -197,6 +237,11 @@ def sample_utility(utility_type: str = 'linear',
             return ThresholdUtility(thresholds_and_ceofficients=utility_repr)
         thresholds_and_coefficients = sample_linear_thresholds()
         return ThresholdUtility(thresholds_and_ceofficients=thresholds_and_coefficients)
+    elif utility_type == "target":
+        if utility_repr is not None:
+            return TargetUtility(target=utility_repr)
+        target = sample_target()
+        return TargetUtility(target=target)
 
     raise ValueError(
         'Expected argument "utility_type" to be a string representing a valid implemented utility')
