3daab6d579b97a6532e0cf98a786934d3208132c
FMalerba <malerbafede@gmail.com>
2021-05-22 18:25:48 +0200

    Minor fixes and added support to compute percentage of times that the correct non-dominated vector is chosen by the agent. Boolean flag added to skip this computation for non-fixed environment results.

--- jupyter_notebooks/utils.py
+++ jupyter_notebooks/utils.py
@@ -52,6 +52,56 @@ ENV_DICT = dict([("{}{}{}{}env".format("cum_" * cum_env, utility + "_", (samplin
                  if not (lin_thresh and (not "threshold" in utility))
                  if not (sampling and ("target" in utility))])
 
+FIXED_ENV_UTILITIES = {
+    "dual_threshold": [
+        DualThresholdUtility(dual_thresholds_and_coefficients=np.array(
+            [[31, 31, dual_thresh0, dual_thresh1, dual_thresh2, dual_thresh3], [-1, -5, r0, r1, r2, r3]],
+            dtype=np.float32)) for dual_thresh0 in range(3) for dual_thresh1 in range(3)
+        for dual_thresh2 in range(3) for dual_thresh3 in range(3) for r0 in np.arange(-20, 21, step=6)
+        for r1 in np.arange(-20, 21, step=6) for r2 in np.arange(-20, 21, step=6)
+        for r3 in np.arange(-20, 21, step=6)
+        if (r0 > 0) or (r1 > 0) or (r2 > 0) or (r3 > 0)
+    ],
+    "linear_dual_threshold": [
+        DualThresholdUtility(dual_thresholds_and_coefficients=np.array(
+            [[31, 31, 31, 31, 31, 31], [-1, -5, r0, r1, r2, r3]], dtype=np.float32))
+        for r0 in np.arange(-20, 21, step=2) for r1 in np.arange(-20, 21, step=2)
+        for r2 in np.arange(-20, 21, step=2)
+        for r3 in np.arange(-20, 21, step=2)
+        if (r0 > 0) or (r1 > 0) or (r2 > 0) or (r3 > 0)
+    ],
+    "linear": [
+        LinearUtility(weights=np.array([-1, -5, r0, r1, r2, r3], dtype=np.float32))
+        for r0 in np.arange(-20, 21, step=2) for r1 in np.arange(-20, 21, step=2)
+        for r2 in np.arange(-20, 21, step=2)
+        for r3 in np.arange(-20, 21, step=2)
+        if (r0 > 0) or (r1 > 0) or (r2 > 0) or (r3 > 0)
+    ],
+    "threshold": [
+        ThresholdUtility(thresholds_and_coefficients=np.array(
+            [[0, 0, thresh0, thresh1, thresh2, thresh3], [-1, -5, r0, r1, r2, r3]], dtype=np.float32))
+        for thresh0 in range(3) for thresh1 in range(3) for thresh2 in range(3) for thresh3 in range(3)
+        for r0 in np.arange(-20, 21, step=6) for r1 in np.arange(-20, 21, step=6)
+        for r2 in np.arange(-20, 21, step=6)
+        for r3 in np.arange(-20, 21, step=6)
+        if (r0 > 0) or (r1 > 0) or (r2 > 0) or (r3 > 0)
+    ],
+    "linear_threshold": [
+        ThresholdUtility(thresholds_and_coefficients=np.array([[0, 0, 0, 0, 0, 0], [-1, -5, r0, r1, r2, r3]],
+                                                              dtype=np.float32))
+        for r0 in np.arange(-20, 21, step=2) for r1 in np.arange(-20, 21, step=2)
+        for r2 in np.arange(-20, 21, step=2)
+        for r3 in np.arange(-20, 21, step=2)
+        if (r0 > 0) or (r1 > 0) or (r2 > 0) or (r3 > 0)
+    ],
+    "target": [
+        TargetUtility(target=np.array([31, 31, target0, target1, target2, target3], dtype=np.float32))
+        for target0 in range(4) for target1 in range(4) for target2 in range(3)
+        for target3 in range(4)
+        if (target0 > 0) or (target1 > 0) or (target2 > 0) or (target3 > 0)
+    ]
+}
+
 
 def load_results(path: Path) -> Dict[str, np.ndarray]:
     keys = set([file.name for file in path.iterdir() if (file.is_dir() and (len(list(file.iterdir())) == 6))])
@@ -118,6 +168,21 @@ def sorting(key: str) -> int:
     return env_sorting[env] * (max(model_sorting.values()) + 1) + model_sorting[model]
 
 
+def match_utility_to_fixed_env(env: str) -> List[UtilityFunction]:
+    if env in LINEAR_TO_SETTINGS.intersection(LINEAR_DUAL_THRESHOLD_SETTINGS.union(DUAL_THRESHOLD_SETTINGS)):
+        return FIXED_ENV_UTILITIES["linear_dual_threshold"]
+    elif env in LINEAR_DUAL_THRESHOLD_SETTINGS.union(DUAL_THRESHOLD_SETTINGS):
+        return FIXED_ENV_UTILITIES["dual_threshold"]
+    elif env in LINEAR_TO_SETTINGS.intersection(LINEAR_THRESHOLD_SETTINGS.union(THRESHOLD_SETTINGS)):
+        return FIXED_ENV_UTILITIES["linear_threshold"]
+    elif env in LINEAR_THRESHOLD_SETTINGS.union(THRESHOLD_SETTINGS):
+        return FIXED_ENV_UTILITIES["threshold"]
+    elif env in LINEAR_SETTINGS:
+        return FIXED_ENV_UTILITIES["linear"]
+    elif env in TARGET_SETTINGS:
+        return FIXED_ENV_UTILITIES["target"]
+
+
 def domination_metric(u: np.ndarray, v: np.ndarray) -> float:
     """Used to compute whether the reward vector u dominates v, viceversa, or neither.
     Do note that this is not a metric in the mathematical sense of the word, but this
@@ -159,6 +224,23 @@ def compute_non_dominated(unique: np.ndarray) -> np.ndarray:
             is_non_dominated[i + 1 + np.argwhere(i_range == 1)] = 0
 
     return is_non_dominated
+
+
+def check_correct_non_dominated(env: str, unique:np.ndarray, inverse: np.ndarray, is_non_dominated: np.ndarray) -> np.ndarray:
+    non_dom_indices = np.argwhere(is_non_dominated)
+    non_dom_vectors = unique[is_non_dominated]
+    indices_to_test = np.isin(inverse, non_dom_indices)
+    
+    utility_functions = match_utility_to_fixed_env(env=env)
+    assert inverse.shape[0] == len(utility_functions), (inverse.shape[0], len(utility_functions))
+    utility_functions_to_test = [f for i, f in enumerate(utility_functions) if indices_to_test[i]]
+    original_inputs = unique[inverse[indices_to_test]][:, np.newaxis]
+    competitors = np.repeat(non_dom_vectors[np.newaxis], np.sum(indices_to_test, dtype=int), axis=0)
+    test_inputs = np.concatenate((original_inputs, competitors), axis=1)
+    assert len(utility_functions_to_test) == test_inputs.shape[0]
+    outputs = np.array([f(x) for f, x in zip(utility_functions_to_test, test_inputs)])
+    assert outputs.shape == test_inputs.shape[:-1], (outputs.shape, test_inputs.shape[:-1], env)
+    return np.mean(np.argmax(outputs, axis=1) == 0)
 
 
 def wds_tables(results: Dict[str, List[np.ndarray]]) -> Tuple[pd.DataFrame]:
@@ -224,40 +306,54 @@ def utilities_table(results: Dict[str, List[np.ndarray]]) -> pd.DataFrame:
     return df
 
 
-def uniques_non_dom_table(results: Dict[str, List[np.ndarray]]) -> pd.DataFrame:
+def uniques_non_dom_table(results: Dict[str, List[np.ndarray]],
+                          correct_non_dom_flag: bool = True) -> pd.DataFrame:
     keys = sorted(results.keys(), key=sorting)
     df = pd.DataFrame(
-        columns=["Setting", "Model", "Uniques", "Non-Dominated", "Non-Dominated %"])
+        columns=["Setting", "Model", "Uniques", "Non-Dominated", "Non-Dominated %"] + ["Correct Non-Dominated"]*correct_non_dom_flag)
 
     for key in tqdm(keys):
         env, model = key.split("-")
 
-        uniques_counts = [np.unique(result, axis=0, return_counts=True) for result in results[key]]
+        uniques_inverse_counts = [np.unique(result, axis=0, return_inverse=True, return_counts=True)
+                                  for result in results[key]]
 
         non_dominated = []
         non_dominated_perc = []
-        for unique, counts in uniques_counts:
+        correct_non_dom = []
+        for unique, inverse, counts in uniques_inverse_counts:
             is_non_dominated = compute_non_dominated(unique=unique * [-1, -1, 1, 1, 1, 1])
+            if correct_non_dom_flag:
+                correct_non_dom.append(check_correct_non_dominated(env=env, unique=unique, inverse=inverse,
+                                                                   is_non_dominated=is_non_dominated))
             non_dominated_perc.append(np.sum(counts[is_non_dominated]) / np.sum(counts))
             non_dominated.append(np.sum(is_non_dominated))
 
-        uniques_shapes = [unique.shape[0] for unique, counts in uniques_counts]
+        uniques_shapes = [unique.shape[0] for unique, inverse, counts in uniques_inverse_counts]
+        
         mean_val_uniques = np.round(np.mean(uniques_shapes), 1)
         std_err_uniques = np.round(np.std(uniques_shapes) / np.sqrt(len(uniques_shapes)), 2)
+        
         mean_val_non_dom = np.round(np.mean(non_dominated), 1)
         std_err_non_dom = np.round(np.std(non_dominated) / np.sqrt(len(non_dominated)), 2)
-        mean_val_non_dom_perc = np.round(np.mean(non_dominated_perc), 5)
+        
+        mean_val_non_dom_perc = np.round(np.mean(non_dominated_perc), 2)
         std_err_non_dom_perc = np.round(np.std(non_dominated_perc) / np.sqrt(len(non_dominated_perc)), 2)
+        
+        if correct_non_dom_flag:
+            mean_val_correct_non_dom = np.round(np.mean(correct_non_dom), 2)
+            std_err_correct_non_dom = np.round(np.std(correct_non_dom) / np.sqrt(len(correct_non_dom)), 2)
 
-        df = df.append(
-            {
-                "Setting": env,
-                "Model": model,
-                "Uniques": f"{mean_val_uniques} (+-{std_err_uniques})",
-                "Non-Dominated": f"{mean_val_non_dom} (+-{std_err_non_dom})",
-                "Non-Dominated %": f"{mean_val_non_dom_perc} (+-{std_err_non_dom_perc})"
-            },
-            ignore_index=True)
+        row = {
+            "Setting": env,
+            "Model": model,
+            "Uniques": f"{mean_val_uniques} (+-{std_err_uniques})",
+            "Non-Dominated": f"{mean_val_non_dom} (+-{std_err_non_dom})",
+            "Non-Dominated %": f"{mean_val_non_dom_perc} (+-{std_err_non_dom_perc})"
+            }
+        if correct_non_dom_flag:
+            row["Correct Non-Dominated"] = f"{mean_val_correct_non_dom} (+-{std_err_correct_non_dom})"
+        df = df.append(row, ignore_index=True)
 
     df['Setting'] = pd.Categorical(df["Setting"], ENVS)
     df['Model'] = pd.Categorical(df["Model"], MODELS)
@@ -365,7 +461,7 @@ TARGET_SETTINGS = {'Cumulative Rewards Target', 'Target'}
 LINEAR_TO_SETTINGS = set([
     "{}{}Linear to {}".format("Cumulative Rewards " * cum_env, SAMPLINGS_DICT[sampling],
                               UTILITIES_DICT[utility])
-    for utility in ["linear", "threshold", "linear_threshold", "dual_threshold", "linear_dual_threshold"]
+    for utility in ["threshold", "linear_threshold", "dual_threshold", "linear_dual_threshold"]
     for sampling in SAMPLINGS
     for cum_env in CUM_ENVS
 ])
